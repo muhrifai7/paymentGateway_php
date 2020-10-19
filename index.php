c22c8d6d90cd7fb6e8e6e94f0358a232e4f227ad
muh rifai <48577618+muhrifai7@users.noreply.github.com>
2020-10-19 11:13:53 +0700

    add modal

--- index.php
+++ index.php
@@ -49,6 +49,27 @@
 </head>
 
 <body>
+<!-- Modal -->
+<div class="modal fade" id="basicExampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
   <!-- Navbar -->
   <nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
@@ -139,7 +160,7 @@
 
                 <h4 class="font-weight-bold blue-text">
                   <strong>Rp 120.000</strong>
-                   <button id='checkout-button'>Checkout</button>
+                   <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#basicExampleModal">Checkout</button>
                 </h4>
 
               </div>
